0d2b927242ddae35b42085f3e850ffe89b226ae6
Measure Space <MeasureSpace2020@gmail.com>
2026-02-27 16:03:58 -0500

    fix push

--- src/measure_space_api/constants.py
+++ src/measure_space_api/constants.py
@@ -1,11 +1,11 @@
 
 URL_MAPPING = {
-    'hourly_weather': 'https://q0np6mu0vi.execute-api.us-east-1.amazonaws.com/prd/global-hourly-weather-forecast',
-    'daily_weather': 'https://4y0sy5lved.execute-api.us-east-1.amazonaws.com/prd/global-daily-weather-forecast',
-    'daily_climate': 'https://d43arqqeh8.execute-api.us-east-1.amazonaws.com/prd/global-daily-climate-forecast',
-    'hourly_air_quality': 'https://pawsqe3sob.execute-api.us-east-1.amazonaws.com/prd/global-hourly-air-quality-forecast',
-    'daily_air_quality': 'https://pawsqe3sob.execute-api.us-east-1.amazonaws.com/prd/global-daily-air-quality-forecast',
-    'geocoding_nearest_city': 'https://ncstsm9hel.execute-api.us-east-1.amazonaws.com/prd/nearest-city',
+    'hourly_weather': 'https://api.measurespace.io/hourly-weather/global-hourly-weather-forecast',
+    'daily_weather': 'https://api.measurespace.io/daily-weather/global-daily-weather-forecast',
+    'daily_climate': 'https://api.measurespace.io/climate/global-daily-climate-forecast',
+    'hourly_air_quality': 'https://api.measurespace.io/air-quality/global-hourly-air-quality-forecast',
+    'daily_air_quality': 'https://api.measurespace.io/air-quality/global-daily-air-quality-forecast',
+    'geocoding_nearest_city': 'https://ncstsm9hel.execute-api.us-east-1.amazonaws.com/geocoding/nearest-city',
     'geocoding_autocomplete': 'https://ncstsm9hel.execute-api.us-east-1.amazonaws.com/prd/autocomplete',
     'pollen': 'https://api.measurespace.io/pollen/global-daily-pollen-forecast',
     'growing_degree_days': 'https://api.measurespace.io/agriculture/growing-degree-days',
